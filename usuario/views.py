a7147015a5eefbb07543a0db30bc95ed2832a11a
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2023-12-04 21:45:13 -0300

    tag extends.

--- usuario/views.py
+++ usuario/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "usuario/index.html")
-
-
-def postularse(request):
-    pass
-
-
-
-def empresas(request):
-    pass
+    return render(request,"usuario/index.html")
 
 def contacto(request):
-    pass
+    return render(request,"usuario/contacto.html")
+
+# def postularse(request):
+#     pass
+
+
+
+# def empresas(request):
+#     pass
