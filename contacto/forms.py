bbcc7ce37c419d295448112c541b84f0cb1388b7
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2023-12-15 19:57:15 -0300

    formulario usando forms.Form

--- contacto/forms.py
+++ contacto/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from .models import Contacto
-# from .models import Contacto
 
-# class FormContacto(forms.Form):
-#     nombre= forms.CharField(max_length=30)
-#     email= forms.EmailField()
-#     telefono= forms.IntegerField()
-#     asunto= forms.CharField(max_length=40)
-#     mensaje= forms.Textarea()
+class FormContacto(forms.Form):
+    nombre= forms.CharField(max_length=30)
+    email= forms.EmailField()
+    telefono= forms.IntegerField()
+    asunto= forms.CharField(max_length=40)
+    mensaje= forms.CharField(max_length=100)
     
 
-class FormContacto(forms.ModelForm):
-    class Meta:
-        model = Contacto
-        fields= "__all__"
+### De esta forma tambien funciona
+# class FormContacto(forms.ModelForm):
+#     class Meta:
+#         model = Contacto
+#         fields= "__all__"
