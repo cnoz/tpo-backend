20eabf800e5b4b58fae50fe91a5c1cd47cadff1b
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2023-12-17 15:49:06 -0300

    form agregue funcion para numero default

--- contacto/forms.py
+++ contacto/forms.py
@@ -7,7 +7,10 @@ class FormContacto(forms.Form):
     telefono= forms.IntegerField()
     asunto= forms.CharField(max_length=40)
     mensaje= forms.CharField(max_length=100)
-    
+        
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['telefono'].initial = 54
 
 ### De esta forma tambien funciona
 # class FormContacto(forms.ModelForm):
